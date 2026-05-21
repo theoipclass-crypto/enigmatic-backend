a9e5620f4e5866c0ddf1add0e9505f8b6d4d2610
theoipclass-crypto <theo.ipclass@gmail.com>
2026-05-21 12:01:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const { ethers } = require("ethers");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://3nigmatic.xyz");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 const allowedOrigins = [
   "https://3nigmatic.xyz",
   "https://www.3nigmatic.xyz",
